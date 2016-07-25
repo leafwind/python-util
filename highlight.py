1d2e0f863950dc9ad6053196c0aabf84ebd83c86
Connor Hsu <leafwind.cs@gmail.com>
2016-07-25 19:02:15 +0800

    Create highlight.py

---
+++ highlight.py
@@ -0,0 +1,14 @@
+def highlight(string, color):
+    ''' return color string for printing to terminal. '''
+    color_mapping = {
+        'red': '31',
+        'green': '32',
+        'yellow': '33',
+        'blue': '34',
+        'purple': '35',
+    }
+    if color not in color_mapping:
+        return string
+    else:
+        attr = color_mapping[color]
+        return '\x1b[{}m{}\x1b[0m'.format(attr string)
