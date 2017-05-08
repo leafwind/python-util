d295b0be4ee6b180531371c578a3dcbd99cb665a
connorhsu <connorhsu@appier.com>
2017-05-08 11:06:01 +0000

    add try-catch decorator

--- utils.py
+++ utils.py
@@ -4,3 +4,9 @@ def file2str(filename):
     with open(filename, 'r') as f:
         data = f.read().replace('\n', '')
         return data
+
+# http://ot-note.logdown.com/posts/67571/-decorator-with-without-arguments-in-function-class-form
+# try:
+#     do_stuff()
+# except Exception as e:
+#     logging.exception('exception: %s', repr(e))
