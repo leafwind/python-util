361fcbb18536ed37f8a155d294c20960535dbf3f
connorhsu <connorhsu@appier.com>
2017-05-09 07:03:50 +0000

    add 2 decorators

--- utils.py
+++ utils.py
@@ -1,12 +1,33 @@
 # -*- coding: utf-8 -*-
 
+import logging
+import time
+
 def file2str(filename):
     with open(filename, 'r') as f:
         data = f.read().replace('\n', '')
         return data
 
-# http://ot-note.logdown.com/posts/67571/-decorator-with-without-arguments-in-function-class-form
-# try:
-#     do_stuff()
-# except Exception as e:
-#     logging.exception('exception: %s', repr(e))
+
+def printException(f):
+    def d_f(*args, **kwargs):
+        try:
+            result = f(*args, **kwargs)
+            return result
+        except Exception as e:
+            logging.exception('exception: %s', repr(e))
+            return None
+    return d_f
+
+
+def timeit(f):
+    def timed(*args, **kwargs):
+        ts_start = time.time()
+        result = f(*args, **kwargs)
+        ts_end = time.time()
+        logging.warning('[timeit] %r (%r, %r) %2.2f sec.', f.__name__, args, kwargs, ts_end - ts_start)
+        return result
+    return timed
+
+
+
